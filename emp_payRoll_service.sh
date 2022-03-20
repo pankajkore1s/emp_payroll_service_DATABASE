1840117ed4c0cc7d501098b378ed7f7177b0553a
pankaj kore <korep95@gmail.com>
2022-03-20 13:34:00 +0530

    [pankaj]created the empRoll Table To manage empPayRolls

--- emp_payRoll_service.sh
+++ emp_payRoll_service.sh
@@ -25,3 +25,27 @@ mysql> select database();
 | employee_payroll_service_db |
 +-----------------------------+
 1 row in set (0.00 sec)
+
+
+#UC2-Ability to create a employee payroll table in the payroll service database to manage employee payrolls
+
+mysql> create table employee_payroll
+    -> (
+    -> id          int unsigned not null auto_increment,
+    -> name        varchar(150) not null,
+    -> salary      double not null,
+    -> start       date not null,
+    -> primary key (id)
+    -> );
+Query OK, 0 rows affected (0.09 sec)
+
+mysql> describe employee_payroll;
++--------+--------------+------+-----+---------+----------------+
+| Field  | Type         | Null | Key | Default | Extra          |
++--------+--------------+------+-----+---------+----------------+
+| id     | int unsigned | NO   | PRI | NULL    | auto_increment |
+| name   | varchar(150) | NO   |     | NULL    |                |
+| salary | double       | NO   |     | NULL    |                |
+| start  | date         | NO   |     | NULL    |                |
++--------+--------------+------+-----+---------+----------------+
+4 rows in set (0.01 sec)
